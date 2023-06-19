c9aa58ef1217be0e38ce756750da3626b24d8474
Andrea Betancourt <111385731+AndreaBetan@users.noreply.github.com>
2023-06-19 18:31:41 +0000

    Good practices

--- src/app.js
+++ src/app.js
@@ -6,24 +6,39 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
+  //write your code here
 
-  let randomWho = who[Math.floor(Math.random() * who.length)];
-  let randomAction = action[Math.floor(Math.random() * action.length)];
-  let randomWhat = what[Math.floor(Math.random() * what.length)];
-  let randomWhen = when[Math.floor(Math.random() * when.length)];
+  function generatorExcuse() {
+    // variables para generar excusas aleatorias.
+    let randomWho = Math.floor(Math.random() * who.length);
+    let randomAction = Math.floor(Math.random() * action.length);
+    let randomWhat = Math.floor(Math.random() * what.length);
+    let randomWhen = Math.floor(Math.random() * when.length);
 
-  let excuseFinal =
-    randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+    return (
+      who[randomWho] +
+      " " +
+      action[randomAction] +
+      " " +
+      what[randomWhat] +
+      " " +
+      when[randomWhen]
+    );
+  };
 
-  document.querySelector("#excuse").innerHTML = excuseFinal;
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+let excuseFinal = generatorExcuse(who, action, what, when);
+
+document.querySelector("#excuse").innerHTML = excuseFinal;
+
 };
