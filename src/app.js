3542ebb623b4caccdc2dc35c0a6390b06ad40d67
Andrea Betancourt <111385731+AndreaBetan@users.noreply.github.com>
2023-06-26 19:56:35 +0000

    parameter inclusion

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
 
-  function generatorExcuse() {
+  function generatorExcuse(who, action, what, when) {
     // variables para generar excusas aleatorias.
     let randomWho = Math.floor(Math.random() * who.length);
     let randomAction = Math.floor(Math.random() * action.length);
@@ -24,21 +24,20 @@ window.onload = function() {
       " " +
       when[randomWhen]
     );
-  };
+  }
 
-let who = ["The dog", "My grandma", "His turtle", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "the keys", "the car"];
-let when = [
-  "before the class",
-  "right on time",
-  "when I finished",
-  "during my lunch",
-  "while I was praying"
-];
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-let excuseFinal = generatorExcuse(who, action, what, when);
+  let excuseFinal = generatorExcuse(who, action, what, when);
 
-document.querySelector("#excuse").innerHTML = excuseFinal;
-
+  document.querySelector("#excuse").innerHTML = excuseFinal;
 };
